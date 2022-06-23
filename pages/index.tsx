2788438ea6571a7af3ba8ad3abc72e32720f26e5
simutis <54304022+SimasG@users.noreply.github.com>
2022-06-23 11:21:35 +0300

    laid out html for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,8 +24,9 @@ const Home: NextPage = () => {
               <h4></h4>
             </div>
           </section>
-          {/* Theme toggle & hide sidebar section */}
+          {/* Log in/out btn + theme toggle + hide sidebar section */}
           <section>
+            <button>Log In</button>
             {/* Theme toggle */}
             <div>
               <img src="" alt="toggle light theme" />
@@ -43,6 +44,119 @@ const Home: NextPage = () => {
           </section>
         </nav>
       </div>
+      {/* Main */}
+      <main>
+        {/* Top Settings */}
+        <section>
+          <h1>Board Name</h1>
+          <button>+ Add New Task</button>
+          <button>Delete Board</button>
+        </section>
+        {/* Main content */}
+        <section>
+          {/* First Column */}
+          <div>
+            {/* Column Title Container */}
+            <div>
+              {/* Colorful circle */}
+              <div></div>
+              {/* Column Title */}
+              <h3>Todo (4)</h3>
+            </div>
+            {/* Task Container */}
+            <div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for xyz</h2>
+                <span>0 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for zyx</h2>
+                <span>0 of 7 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for abc</h2>
+                <span>1 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for cba</h2>
+                <span>2 of 5 subtasks</span>
+              </div>
+            </div>
+          </div>
+          {/* Second Column */}
+          <div>
+            {/* Column Title Container */}
+            <div>
+              {/* Colorful circle */}
+              <div></div>
+              {/* Column Title */}
+              <h3>Doing (4)</h3>
+            </div>
+            {/* Task Container */}
+            <div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for xyz</h2>
+                <span>0 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for zyx</h2>
+                <span>0 of 7 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for abc</h2>
+                <span>1 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for cba</h2>
+                <span>2 of 5 subtasks</span>
+              </div>
+            </div>
+          </div>
+          {/* Third Column */}
+          <div>
+            {/* Column Title Container */}
+            <div>
+              {/* Colorful circle */}
+              <div></div>
+              {/* Column Title */}
+              <h3>Done (4)</h3>
+            </div>
+            {/* Task Container */}
+            <div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for xyz</h2>
+                <span>0 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for zyx</h2>
+                <span>0 of 7 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for abc</h2>
+                <span>1 of 3 subtasks</span>
+              </div>
+              {/* Single Task Container */}
+              <div>
+                <h2>Example todo for cba</h2>
+                <span>2 of 5 subtasks</span>
+              </div>
+            </div>
+          </div>
+          {/* Add New Column btn */}
+          <div>+ New Column</div>
+        </section>
+      </main>
     </div>
   );
 };
