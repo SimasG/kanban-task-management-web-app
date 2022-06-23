b147f08926c3c75a6257fa4fceede02738fa5a83
simutis <54304022+SimasG@users.noreply.github.com>
2022-06-23 11:06:07 +0300

    set up project colors & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Will need to add colors for the bright theme as well
+      colors: {
+        fontPrimary: "#fff",
+        fontSecondary: "#686E7D",
+        fontTertiary: "#645FC6",
+        darkBlue: "#21212D",
+        darkGray: "#2c2c38",
+        todoColors: {
+          brightBlue: "#4fc4ef",
+          violet: "#645FC6",
+          brightGreen: "#67e4ac",
+          yellow: "#FAD201",
+          brightRedOrange: "#F75E25",
+          purpleViolet: "#4A192C",
+        },
+      },
+      fontFamily: {
+        body: ["Roboto"],
+      },
+    },
   },
   plugins: [],
-}
+};
