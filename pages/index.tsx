0faf5b005faa76895bbfb394ffcd8c553fa173e7
simutis <54304022+SimasG@users.noreply.github.com>
2022-06-23 11:40:46 +0300

    added icons to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -14,13 +16,22 @@ const Home: NextPage = () => {
             <h3></h3>
             {/* Boards subcontainer */}
             <div>
-              <img src="" alt="board icon" />
-              {/* Individual Board name */}
-              <h4></h4>
+              {/* Specific Board */}
+              <div>
+                <MdOutlineSpaceDashboard />
+                {/* Individual Board name */}
+                <h4>Platform Launch</h4>
+              </div>
+              {/* Specific Board */}
+              <div>
+                <MdOutlineSpaceDashboard />
+                {/* Individual Board name */}
+                <h4>Marketing Plan</h4>
+              </div>
             </div>
             {/* Create new Board container */}
             <div>
-              <img src="" alt="board icon" />
+              <MdOutlineSpaceDashboard />
               <h4></h4>
             </div>
           </section>
@@ -29,16 +40,57 @@ const Home: NextPage = () => {
             <button>Log In</button>
             {/* Theme toggle */}
             <div>
-              <img src="" alt="toggle light theme" />
+              {/* Toggle light theme icon */}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                ></path>
+              </svg>
               {/* Rectangle */}
               <div>
                 {/* Circle */}
                 <div></div>
               </div>
-              <img src="" alt="toggle dark theme" />
+              {/* Toggle dark theme icon */}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                ></path>
+              </svg>{" "}
             </div>
             <div>
-              <img src="" alt="hide sidebar icon" />
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                ></path>
+              </svg>
               <h4>Hide Sidebar</h4>
             </div>
           </section>
