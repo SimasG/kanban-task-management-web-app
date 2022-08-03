04074fc2b907ada9853aa53b4deb3778729f15de
simutis <54304022+SimasG@users.noreply.github.com>
2022-08-03 13:30:44 +0300

    made adjustments for deployment

--- pages/boards/[boardId]/index.tsx
+++ pages/boards/[boardId]/index.tsx
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 const index = () => {
-  const router = useRouter();
-  const boardId = router.query.boardId;
-  return <div>Board id: {boardId}</div>;
+  // const router = useRouter();
+  // const boardId = router.query.boardId;
+  return <div>Board id: *will be implemented soon*</div>;
 };
 
 export default index;
