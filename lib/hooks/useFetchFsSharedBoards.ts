13b6891464b08fe74bb79900f52488091af6a505
simutis <54304022+SimasG@users.noreply.github.com>
2022-08-11 19:17:02 +0300

    fetched shared Boards

--- lib/hooks/useFetchFsSharedBoards.ts
+++ lib/hooks/useFetchFsSharedBoards.ts
@@ -1,5 +1,13 @@
 import { User } from "firebase/auth";
-import { collection, doc, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  collectionGroup,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { useContext } from "react";
 import {
   useCollectionData,
@@ -8,21 +16,29 @@ import {
 import { UserContext } from "../context";
 import { db } from "../firebase";
 
-const useFetchFsSharedBoards = () => {
+const useFetchFsSharedBoards = async () => {
   const user = useContext(UserContext);
 
-  // ** 1. Fetch Users Doc + Extract sharedBoards
+  // ** 1. Fetch Users Doc + Extract shared Board ids
   const userRef = doc(db, "users", `${user?.uid}`);
-  const sharedBoards = useDocumentData(userRef)[0]?.sharedBoards;
-  if (!sharedBoards) return;
+  const sharedBoardIds = useDocumentData(userRef)[0]?.sharedBoards;
+  if (!sharedBoardIds) return;
+  // console.log("sharedBoardIds:", sharedBoardIds);
+
+  // ** 2. Fetch Boards (CollectionGroup query, where boardId matches one of the extracted boardIds)
+  const sharedBoardsQuery = query(
+    // Collection Group Queries fetch any subcollections with the same name within the tree of nested documents
+    // E.g. now we can run this query for posts without needing the username of every single user who posted
+    collectionGroup(db, "boards"),
+    where("collaborators", "array-contains-any", [`${user?.email}`])
+  );
+
+  const sharedBoards = (await getDocs(sharedBoardsQuery)).docs.map((doc: any) =>
+    doc.data()
+  );
   console.log("sharedBoards:", sharedBoards);
 
-  // ** Fetch Boards with the extracted boardIds (CollectionGroup query, where boardId
-  // matches one of the extracted boardIds)
-
-  const data = "zdare";
-
-  return data;
+  return sharedBoards;
 };
 
 export default useFetchFsSharedBoards;
